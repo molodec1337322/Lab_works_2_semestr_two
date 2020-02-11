894294e643bb88a0096cde923d78a3770d069cae
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-02-11 09:47:10 +0300

    Update lab_1.cpp

--- lab_1.cpp
+++ lab_1.cpp
@@ -1,3 +1,4 @@
+//lab 1
 #include <iostream>
 
 int main()
@@ -8,4 +9,21 @@ int main()
 	std::cout << "Введите кол-во галлонов: ";
 	std::cin >> feets;
 	std::cout << "\nКубических фунтов: " << gallonsToCubicFeets * feets;
+}
+
+
+//lab 2  
+#include <iostream>
+#include "stdlib.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	for (int i = 1990; i < 1994; i++)
+	{
+		cout << i << "\t" << rand() << "\n";
+	}
 }
