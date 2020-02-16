06ab210c01d187aa2f8d50b54fb387e43ff2f4fc
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-02-16 22:02:48 +0300

    Create lab_2.cpp

---
+++ lab_4/lab_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+double power(double n, int p = 2)
+{
+	double result = 1.0;
+	for (int j = 0; j < p; j++)
+	{
+		result *= n;
+	}
+	return result;
+}
+
+int main()
+{
+	double number, answer;
+	int pow;
+	char userResponse;
+	cout << "\nведите число:";
+	cin >> number;
+	cout << "Будете вводить степень (y/n)?";
+	cin >> userResponse;
+	if (userResponse == 'y ')
+	{
+		cout << "Введите степень:";
+		cin >> pow;
+		answer = power(number, pow);
+	}
+	else
+	{
+		answer = power(number);
+	}
+	cout << "ќтвет " << answer << endl;
+	return 0;
+}
