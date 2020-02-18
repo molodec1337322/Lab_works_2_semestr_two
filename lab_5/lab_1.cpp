109f32f7a2e269eea6f55724f6d57ae2696a2b98
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-02-18 09:00:17 +0300

    1

--- lab_5/lab_1.cpp
+++ lab_5/lab_1.cpp
@@ -1,1 +1,44 @@
 
+
+#include <iostream>
+
+using namespace std;
+
+class Int 
+{
+    private:
+      int i;
+              
+    public:
+      Int()                  
+      {
+        i =0;
+      }
+        
+      Int(int num)           
+      {
+        i = num;
+      }
+      
+      void add(Int i_2, Int i_3) 
+      {
+        i = i_2.i + i_3.i;
+      }
+      
+      void display()       
+      {
+        cout << i;
+      }
+};
+
+int main()
+{
+    Int int_1(7);         
+    Int int_2(11);        
+    Int int_3();    
+            
+    int_3.add(int_1, int_2);  
+    
+    cout << "\nInt3 =" << int_3.display(); 
+    cout << endl;
+}
