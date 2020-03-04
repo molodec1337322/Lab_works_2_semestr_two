9aa3443faae1daf53c82d26a23aa83efe1a87aeb
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-03-04 18:58:32 +0300

    Update lab_5.cpp

--- lab_7/lab_5.cpp
+++ lab_7/lab_5.cpp
@@ -1,1 +1,90 @@
+#include <iostream>
 
+using namespace std;
+
+class Time
+{
+private:
+	int hours;
+	int minutes;
+	int seconds;
+
+	void CheckOverflow()
+	{
+		if (seconds >= 60)
+		{
+			seconds -= 60;
+			minutes++;
+		}
+		if (minutes >= 60)
+		{
+			minutes -= 60;
+			hours++;
+		}
+
+		if (seconds < 0)
+		{
+			seconds += 60;
+			minutes--;
+		}
+		if (minutes < 0)
+		{
+			minutes += 60;
+			hours--;
+		}
+	}
+
+public:
+
+	Time() : hours(0), minutes(0), seconds(0)
+	{}
+
+	Time(int hours, int minutes, int seconds) : hours(hours), minutes(minutes), seconds(seconds)
+	{}
+
+	void display() const
+	{
+		cout << hours << ":" << minutes << ":" << seconds;
+	}
+
+	Time& operator++()
+	{
+		++seconds;
+		CheckOverflow();
+		return *this;
+	}
+
+	Time operator++(int)
+	{
+		Time oldVal(hours, minutes, seconds);
+		++(*this);
+		return oldVal;
+	}
+
+	Time& operator--()
+	{
+		--seconds;
+		CheckOverflow();
+		return *this;
+	}
+
+	Time operator--(int)
+	{
+		Time oldVal(hours, minutes, seconds);
+		--(*this);
+		return oldVal;
+	}
+};
+
+
+int main()
+{
+	const Time time(2, 0, 50);
+	
+
+	cout << "time = ";
+	time.display();
+	cout << "\ntime = ";
+	++time.display();
+	cout << endl;
+}
