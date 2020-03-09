9776289677ec3e06bccde3005fb4567d81ea8fa3
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-03-09 14:16:31 +0300

    Update lab_10.cpp

--- lab_7/lab_10.cpp
+++ lab_7/lab_10.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include "math.h"
 
+using namespace std;
+
+class Polar
+{
+private:
+	int x;
+	int y;
+
+	double radius;
+	double angle;
+
+public:
+	Polar(): x(0), y(0), angle(0), radius(x* x + y * y)
+	{}
+
+	Polar(int x, int y): x(x), y(y), radius(sqrt(x* x + y * y)), angle(acos((double)x / (double)radius))
+	{}
+
+	Polar operator + (Polar p)
+	{
+		int newX = x + p.x;
+		int newY = y + p.y;
+
+		return Polar(newX, newY);
+	}
+
+	double GetRadius() const
+	{
+		return radius;
+	}
+
+	double GetAngle() const
+	{
+		return angle;
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Polar p1(0, 5), p2(5, 0);
+	Polar p3 = p1 + p2;
+	cout << "Полярный радиус новой координаты: " << p3.GetRadius() << "\nПолярный угол равен: " << p3.GetAngle();
+}
