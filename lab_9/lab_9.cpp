94af9d936dc8295da15dfd962833b6a48633ce9e
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-04-06 14:49:32 +0300

    Create lab_9.cpp

---
+++ lab_9/lab_9.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+
+class LinkedArrays
+{
+private:
+	int** ap;
+	int size;
+	const static int subSize = 3;
+
+public:
+	LinkedArrays(int size) : size(size)
+	{
+		ap = new int*[size];
+		for (int i = 0; i < size; i++)
+		{
+			int* subArr = new int[subSize];
+			*(ap + i) = subArr;
+		}
+	}
+
+	int& operator()(int j, int k)
+	{
+		return *(*(ap + j) + k);
+	}
+};
+
+
+int main()
+{
+	LinkedArrays arr(2);
+	arr(0, 0) = 0;
+	arr(0, 1) = 1;
+	arr(1, 1) = 10;
+
+	cout << arr(0, 0) << endl << arr(0, 1) << endl << arr(1, 1);
+	return 0;
+}
