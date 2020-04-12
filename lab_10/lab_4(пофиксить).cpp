29110a74d566f542e7a0e5ce6a6849e4b1d2809e
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-04-12 23:33:32 +0300

    Create lab_4(пофиксить).cpp

---
+++ lab_10/lab_4(пофиксить).cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Array 
+{
+private:
+	int* ptr; 
+	int size; 
+
+public:
+	Array(int s) 
+	{
+		size = s; 
+		ptr = new int[s]; 
+	}
+
+	~Array() 
+	{
+		delete[] ptr;
+	}
+
+	int& operator [] (int j) 
+	{
+		return *(ptr + j);
+	}
+
+	Array operator=(Array& arr)
+	{
+		delete[] ptr;
+		ptr = new int[arr.size];
+		size = arr.size;
+		for (int i = 0; i < arr.size; i++)
+		{
+			*(ptr + i) = *(arr.ptr + i);
+		}
+
+		return *this;
+	}
+};
+
+
+int main()
+{
+	const int ASIZE = 10; 
+	Array arr(ASIZE); 
+	Array arrCopy(0);
+
+	for (int j = 0; j < ASIZE; j++) 
+		arr[j] = j * j;
+
+	arrCopy = arr;
+
+	for (int j = 0; j < ASIZE; j++) 
+		cout << arrCopy[j] << ' ';
+
+	cout << endl;
+	return 0;
+}
